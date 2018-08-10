dfa8df4dc2363a546bd2a5bc3f2281638669dfd2
Satya Jugran <satya.jugran@gmail.com>
2018-08-10 17:47:21 +0530

    coursera machine learning (matlab) completed programming assignments

---
+++ ex1/normalEquation.m
@@ -0,0 +1,21 @@
+n to linear 
+%   regression using the normal equations.
+
+theta = zeros(size(X, 2), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the code to compute the closed form solution
+%               to linear regression and put the result in theta.
+%
+
+% ---------------------- Sample Solution ----------------------
+
+
+theta = pinv(X'*X)*X'*y;
+
+% -------------------------------------------------------------
+
+
+% ============================================================
+
+end
